a60e89ce5e7de0f616da37dfb503a08d64f573b9
Younes Elguerraoui <yoelguer@e2r1p5.1337.ma>
2022-02-25 05:15:23 +0100

    MUI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Navbar from './component/navbar/Navbar';
 import './App.css';
 
-
 class App extends Component {
   render() {
     return (
-    <>  
+    <div className="App-container"> 
       <Router>
         <Navbar /> 
         <Routes>
@@ -19,7 +18,7 @@ class App extends Component {
           <Route exact path='/contact' element={< Contact />}></Route>
         </Routes>
     </Router>
-    </>
+    </div>
         
  );  
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import Hero from '../img/hero.jpg';
 import '../App.css';
+import styled from "styled-components";
+import Grid from '@mui/material/Grid';
+
+const Heroo = styled.div`
+  width: 100%;
+  height: 800px;
+  background-image: url(${Hero}); no-repeat;
+  background-size: cover;
+  background-position: center;
+  @media screen and (max-width: 1024px) {
+    width: auto;
+    height: 600px;
+  }
+  @media screen and (max-width: 640px) {
+    width: auto;
+    height: 400px;
+  }
+`;
 
 export default function home() {
   return(
-    <div className="container-hero">
-      <div className="home-hero relative overflow-hidden  shadow-lg cursor-pointer w-full h-2/6 ">
-        <img alt="hero" src={Hero} className="object-cover h-2/6 w-full "/>
-        <div class="absolute top-0  px-40 py-60">
-          <h1 class="mb-3 text-5xl  tracking-tight text-white">WELCOME TO HELL</h1>
-        </div>
+ <div className="container-home">
+      <h1 className="hero-text top0">
+          YOUNES ELGUERRAOUI <br/>
+          <p className="ml-8">softwar</p>
+      </h1>
+      <Heroo />
+      <div className="container-body">
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid item xs={6}>
+              <h1>hello</h1>
+            </Grid>
+            <Grid item xs={6}>
+            <h1>hello</h1>
+            </Grid>
+            <Grid item xs={6}>
+            <h1>hello</h1>
+            </Grid>
+            <Grid item xs={6}>
+            <h1>hello</h1>
+            </Grid>
+          </Grid>
       </div>
-    </div>
+ </div>
+
   )
 }
